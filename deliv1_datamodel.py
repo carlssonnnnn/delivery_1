a2421e52d4dc9dce568574a4d46d68f75552fa9c
Jacob <jaco776s@stud.kea.dk>
2022-03-22 15:05:35 +0100

    Rettelser

--- deliv1_datamodel.py
+++ deliv1_datamodel.py
@@ -8,13 +8,13 @@ import calendar
 # ***************************************
 
 #githubpath = 'https://raw.githubusercontent.com/TueHellsternKea/dashdemo/main/data/'
-#githubpath = './data/'
+githubpath = './data/'
 
 # Import from Excel file, 4 different sheets
-df_customers = pd.read_excel("my_shop_data.xlsx", sheet_name="customers")
-df_order = pd.read_excel("my_shop_data.xlsx", sheet_name="order")
-df_employee = pd.read_excel("my_shop_data.xlsx", sheet_name="employee")
-df_products = pd.read_excel("my_shop_data.xlsx", sheet_name="products")
+df_customers = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="customers")
+df_order = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="order")
+df_employee = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="employee")
+df_products = pd.read_excel(githubpath + "my_shop_data.xlsx", sheet_name="products")
 
 
 def get_data():
